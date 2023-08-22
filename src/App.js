fbe169e42b3a7885a1709adc97c4ff5ab4e6b648
Thinh Nguyen <thinhnguyen1411@gmail.com>
2023-08-22 22:47:28 +0700

    update name

--- src/App.js
+++ src/App.js
@@ -9,14 +9,15 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        <p>Thinh Nguyen</p>
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
