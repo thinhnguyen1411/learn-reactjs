009623d0015eeba6aac7eee2a9f33969203754ef
Thinh Nguyen <thinhnguyen1411@gmail.com>
2023-08-27 13:55:24 +0700

    setup Storage keys

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,15 @@
-import { IconButton } from '@material-ui/core';
+import { Box, IconButton } from '@material-ui/core';
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import { Close } from '@material-ui/icons';
 import CodeIcon from '@material-ui/icons/Code';
-// import Register from 'features/Auth/components/Register';
 import Login from 'features/Auth/components/Login';
+import Register from 'features/Auth/components/Register';
 import { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom/cjs/react-router-dom';
 
@@ -35,11 +34,20 @@ const useStyles = makeStyles((theme) => ({
     color: theme.palette.grey[500],
     zIndex: 1,
   },
+  closeRegisterDiv: {
+    alignItems: 'center',
+    alignContent: 'center',
+  },
 }));
+
+const MODE = {
+  LOGIN: 'login',
+  REGISTER: 'register',
+};
 
 export default function Header() {
   const [open, setOpen] = useState(false);
-
+  const [mode, setMode] = useState(MODE.LOGIN);
   const handleClickOpen = () => {
     setOpen(true);
   };
@@ -87,8 +95,29 @@ export default function Header() {
           <Close />
         </IconButton>
         <DialogContent>
-          {/* <Register closeDialog={handleClose} /> */}
-          <Login closeDialog={handleClose} />
+          <div>
+            {mode === MODE.REGISTER && (
+              <div className={classes.closeRegisterDiv}>
+                <Register closeDialog={handleClose} />
+                <Box textAlign="center">
+                  <Button color="primary" onClick={() => setMode(MODE.LOGIN)}>
+                    Already have an account. Login here!
+                  </Button>
+                </Box>
+              </div>
+            )}
+
+            {mode === MODE.LOGIN && (
+              <div className={classes.closeRegisterDiv}>
+                <Login closeDialog={handleClose} />
+                <Box textAlign="center">
+                  <Button color="primary" onClick={() => setMode(MODE.REGISTER)}>
+                    Don't have an account. Register here!
+                  </Button>
+                </Box>
+              </div>
+            )}
+          </div>
         </DialogContent>
       </Dialog>
     </div>
--- src/features/Auth/userSlice.js
+++ src/features/Auth/userSlice.js
@@ -1,13 +1,13 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import userApi from 'api/userApi';
-
+import StorageKeys from 'constants/storage-keys';
 export const register = createAsyncThunk('user/register', async (payload) => {
   //call api to register
   const data = await userApi.register(payload);
 
   //save data to local storage
-  localStorage.setItem('access_token', data.jwt);
-  localStorage.setItem('user', JSON.stringify(data.user));
+  localStorage.setItem(StorageKeys.TOKEN, data.jwt);
+  localStorage.setItem(StorageKeys.USER, JSON.stringify(data.user));
 
   //return user data
   return data.user;
@@ -18,8 +18,8 @@ export const login = createAsyncThunk('user/login', async (payload) => {
   const data = await userApi.login(payload);
 
   //save data to local storage
-  localStorage.setItem('access_token', data.jwt);
-  localStorage.setItem('user', JSON.stringify(data.user));
+  localStorage.setItem(StorageKeys.TOKEN, data.jwt);
+  localStorage.setItem(StorageKeys.USER, JSON.stringify(data.user));
 
   //return user data
   return data.user;
