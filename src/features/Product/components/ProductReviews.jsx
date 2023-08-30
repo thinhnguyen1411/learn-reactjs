2728c542399c8b51b43ab6c7dcfc1724a0c8a61a
Thinh Nguyen <thinhnguyen1411@gmail.com>
2023-08-30 17:52:58 +0700

    Product description & dompurify

---
+++ src/features/Product/components/ProductReviews.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+ProductReviews.propTypes = {};
+
+function ProductReviews(props) {
+  return <div>ProductReviews</div>;
+}
+
+export default ProductReviews;
--- src/features/Product/pages/DetailPage.jsx
+++ src/features/Product/pages/DetailPage.jsx
@@ -1,10 +1,13 @@
 import { Box, Container, Grid, Paper, makeStyles } from '@material-ui/core';
 import ProductThumbnail from '../components/ProductThumbnail';
-import { useRouteMatch } from 'react-router-dom/cjs/react-router-dom';
+import { Route, useRouteMatch, Switch } from 'react-router-dom/cjs/react-router-dom';
 import useProductDetail from '../hooks/useProductDetail';
 import ProductInfo from '../components/ProductInfo';
 import AddToCartForm from '../components/AddToCartForm';
 import ProductMenu from '../components/ProductMenu';
+import ProductDescription from '../components/ProductDescription';
+import ProductAdditional from '../components/ProductAdditional';
+import ProductReviews from '../components/ProductReviews';
 
 const useStyles = makeStyles((theme) => ({
   root: {},
@@ -23,6 +26,7 @@ function DetailPage() {
   const classes = useStyles();
   const {
     params: { productId },
+    url,
   } = useRouteMatch();
 
   const { loading, product } = useProductDetail(productId);
@@ -50,6 +54,14 @@ function DetailPage() {
           </Grid>
         </Paper>
         <ProductMenu />
+
+        <Switch>
+          <Route exact path={url}>
+            <ProductDescription product={product} />
+          </Route>
+          <Route path={`${url}/additional`} component={ProductAdditional} />
+          <Route path={`${url}/reviews`} component={ProductReviews} />
+        </Switch>
       </Container>
     </Box>
   );
